387f548b85f8c1ca66fc03a1c7292b7c6ec655a5
weicheng <weicheng2138@gmail.com>
2021-08-29 21:22:25 +0800

    add serviceWorker

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import WeatherApp from "./WeatherApp";
+import * as serviceWorker from "./serviceWorker";
 
 // 這支 CSS 檔的樣式會作用到全域
 import "./styles.css";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <StrictMode>
-    <WeatherApp />
-  </StrictMode>,
-  rootElement
+    <StrictMode>
+        <WeatherApp />
+    </StrictMode>,
+    rootElement
 );
+
+serviceWorker.register();
